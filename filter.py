bc9ff3fc8b2c59d8d24107a7a32ba3801c6a641f
Sera Bak <serabak15@gmail.com>
2016-06-20 18:47:20 -0700

    added function filter.py

--- filter.py
+++ filter.py
@@ -1,24 +1,28 @@
 
 
 
-# This program takes a body of text and filters swear words and replaces with more palatable words.
+# This program takes a body of text and replaces swear words with more palatable words.
 
 #Ask user for body of text
-text = "fuck you bitch you little shit"
-print ("original text:", text)
+user_text = "damn fucking bitch. shut the fuck up!"
+print ("original text: " + user_text)
 
 
 # dictionary of bad words and their replacements
 
-bad_word_dict = {'fuck': 'eff', 'bitch': 'biatch', 'shit': 'shiznit'}
+bad_word_dict = {'shut the fuck up': 'shut the front door', 'fuck': 'eff', 'bitch': 'biznatch', 'shit': 'shiznit', 'asshole': 'a-hole', 'damn': 'darn', 'fucking': 'flunking'}
 
-# Loop over dictionary and look for certain words. If bad word exists, replace with another word.
+# Iterate through dictionary and replace all bad words in text.
 
-for key in bad_word_dict:
-	text = text.replace(key, bad_word_dict[key])
-	
+def replace_bad_words(text, dict):
+	for key in dict:
+		text = text.replace(key, dict[key])
+	return text		
 
-print ("new text:", text)
+# Call replace_bad_words function
+
+new_text = replace_bad_words(user_text, bad_word_dict)
+print ("new text: " + new_text.capitalize())
 
 
 
